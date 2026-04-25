1a4338d7fdcbe3ebccaa0bba96ac0a8d502bef80
konstantinwdk <entiplay@gmail.com>
2026-04-25 21:12:19 +0200

    tree js añadido nueva galaxia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 export default defineConfig({
   site: 'https://astro-components.dev',
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+      }
+    }
+  }
 });
